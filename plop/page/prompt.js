9bc3c29aab60f1190e166996fc06bdfe04697202
echofly <byteland@163.com>
2021-09-12 23:56:07 +0800

    init

---
+++ plop/page/prompt.js
@@ -0,0 +1,41 @@
+const { notEmpty, correctPath } = require('../utils.js')
+
+module.exports = {
+  description: '生成一个页面',
+  prompts: [
+    {
+      type: 'input',
+      name: 'name',
+      message: '请输入页面名称:',
+      validate: notEmpty('页面名称'),
+    },
+    {
+      type: 'input',
+      name: 'path',
+      message: '请输入页面存放的路径(src/<your path>):',
+      validate: correctPath('路径'),
+    },
+  ],
+  actions: [
+    {
+      type: 'add',
+      path: 'src/{{path}}/{{name}}/{{name}}.js',
+      templateFile: 'plop/page/index.js.hbs',
+    },
+    {
+      type: 'add',
+      path: 'src/{{path}}/{{name}}/{{name}}.json',
+      templateFile: 'plop/page/index.json.hbs',
+    },
+    {
+      type: 'add',
+      path: 'src/{{path}}/{{name}}/{{name}}.wxml',
+      templateFile: 'plop/commonHbs/index.wxml.hbs',
+    },
+    {
+      type: 'add',
+      path: 'src/{{path}}/{{name}}/{{name}}.wxss',
+      templateFile: 'plop/commonHbs/index.wxss.hbs',
+    },
+  ]
+}
